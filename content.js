7c57f6bffd85b06258169ea2db2c47a0ac30d4d6
William Ravelo <drummerwilliam@gmail.com>
2018-06-05 12:03:35 -0500

    added initial extension code

---
+++ content.js
@@ -0,0 +1,23 @@
+document.body.onkeyup = function(e){
+	if(e.keyCode == 32){
+		var btnPlay = document.getElementById("player-playPause").click();
+		if(btnPlay) {
+			document.getElementById("player-playPause").click()
+		}
+	} else if(e.keyCode == 70) {
+		var btnPlay = document.getElementById("player-playPause").click();
+		var enter = document.getElementById("player-fullscreen");
+		var exit = document.getElementById("player-leave-fullscreen");
+		if(enter && exit && btnPlay) {
+			if(hidden(enter)) {
+				exit.click();
+			} else if(hidden(exit)) {
+				enter.click();
+			}
+		}
+	}
+}
+
+function hidden(element) {
+	return element.offsetWidth === 0 && element.offsetHeight === 0;
+}
